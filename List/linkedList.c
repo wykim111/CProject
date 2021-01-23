08c5eebc1f92d95ef584d487bac4ecf7f080fe95
wykim111 <40811068+wykim111@users.noreply.github.com>
2021-01-23 13:14:40 +0900

    Update linkedList.c

--- List/linkedList.c
+++ List/linkedList.c
@@ -4,8 +4,8 @@
 typedef struct _node
 {
 
-    int data;
-    struct _node* next;
+	int data;
+	struct _node* next;
 
 }stNode;
 
@@ -15,155 +15,155 @@ stNode* tail;
 
 void addNode(int num)
 {
-    stNode* newNode = malloc(sizeof(stNode));
-    
-    if(head->next == NULL)
-    {
-        newNode->data = num;
-        newNode->next = NULL;
+	stNode* newNode =(stNode*)malloc(sizeof(stNode));
 
-        head->next = newNode;
-        tail = newNode;
-    }
-    else
-    {
-        newNode->data = num;
-        newNode->next = NULL;
+	if (head->next == NULL)
+	{
+		newNode->data = num;
+		newNode->next = NULL;
 
-        tail->next = newNode;
-        tail = newNode;
+		head->next = newNode;
+		tail = newNode;
+	}
+	else
+	{
+		newNode->data = num;
+		newNode->next = NULL;
 
-    }
+		tail->next = newNode;
+		tail = newNode;
+
+	}
 
 }
 
 void iterList()
 {
-    stNode* cur = head->next;
+	stNode* cur = head->next;
 
-    if (cur == NULL)
-    {
-        printf("no valid\n");
-        return;
-    }
+	if (cur == NULL)
+	{
+		printf("no valid\n");
+		return;
+	}
 
-    while(cur != NULL)
-    {
-        printf("%d\n",cur->data);
-        cur = cur->next;
+	while (cur != NULL)
+	{
+		printf("%d\n", cur->data);
+		cur = cur->next;
 
-    }
+	}
 
 }
 
 int removeNode(int rmData)
 {
-    stNode* delNode = head->next;
-    stNode* preNode = head;
+	stNode* delNode = head->next;
+	stNode* preNode = head;
 
-    if(delNode == NULL)
-    {
-        printf("unValid Node\n");
-        return -1;
-    }
+	if (delNode == NULL)
+	{
+		printf("unValid Node\n");
+		return -1;
+	}
 
-    while(delNode != NULL)
-    {
-        if(delNode-> data == rmData)
-        {
-            printf("success remove Data\n");
+	while (delNode != NULL)
+	{
+		if (delNode->data == rmData)
+		{
+			printf("success remove Data\n");
 
-            preNode->next = delNode->next;
-            free(delNode);
-            return 1;
-            
-        }
-        preNode = delNode;
-        delNode = delNode->next;
-    }
-        
-    return 0;
+			preNode->next = delNode->next;
+			free(delNode);
+			return 1;
+
+		}
+		preNode = delNode;
+		delNode = delNode->next;
+	}
+
+	return 0;
 }
 
 int searchNode(int target)
 {
-    stNode* curNode = head->next;
+	stNode* curNode = head->next;
 
-    if(curNode == NULL)
-    {
-        printf("No Target %d\n",target);
-        return -1;
-    }
+	if (curNode == NULL)
+	{
+		printf("No Target %d\n", target);
+		return -1;
+	}
 
-    while(curNode != NULL)
-    {
-        if(curNode->data == target)
-        {
-            printf("success target :%d\n",target);
-            return 1;
+	while (curNode != NULL)
+	{
+		if (curNode->data == target)
+		{
+			printf("success target :%d\n", target);
+			return 1;
 
-        }
+		}
 
-        curNode = curNode->next;
+		curNode = curNode->next;
 
-    }
+	}
 
-    printf("fail target:%d\n",target);
+	printf("fail target:%d\n", target);
 
 
-    return 0;
+	return 0;
 }
 
 
 void deleteMem(stNode* delNode)
 {
-    stNode* curNode = delNode;
+	stNode* curNode = delNode;
 
-    if(delNode == NULL)
-    {
-        printf("empty Node\n");
+	if (delNode == NULL)
+	{
+		printf("empty Node\n");
 
-        return;
-    }
+		return;
+	}
 
 
-    while(curNode != NULL)
-    {
-        delNode = curNode;
-        curNode = curNode->next;
-        free(delNode);
-    }
+	while (curNode != NULL)
+	{
+		delNode = curNode;
+		curNode = curNode->next;
+		free(delNode);
+	}
 
 }
 
 int main()
 {
-   int cnt;
-   int num;
+	int cnt;
+	int num;
 
-   head = (stNode*)malloc(sizeof(stNode));
+	head = (stNode*)malloc(sizeof(stNode));
 
-   scanf("%d",&cnt);
+	scanf("%d", &cnt);
 
-    for(int i=0;i<cnt;i++)
-    {
-        scanf("%d",&num);
+	for (int i = 0; i < cnt; i++)
+	{
+		scanf("%d", &num);
 
-        addNode(num);
-    }
-
-
-    iterList();
-    
-    //제거할 데이터
-    removeNode(4);
-
-    searchNode(9);
-
-    iterList();
+		addNode(num);
+	}
 
 
-    deleteMem(head);
+	iterList();
 
-    return 0;
+	//제거할 데이터
+	removeNode(4);
+
+	searchNode(9);
+
+	iterList();
+
+
+	deleteMem(head);
+
+	return 0;
 }
